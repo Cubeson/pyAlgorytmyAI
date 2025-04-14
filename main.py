7acb7031183ee384ddbce7a5f7a2e1356b65a552
Cubeson <jakuble@onet.pl>
2025-04-14 17:28:17 +0200

    refactor zapisu xlsx,

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import numpy as np
 import xlsxwriter
-import datetime
+import time
 import shutil
 import csv
 from mealpy import FloatVar, ArchOA, ASO, CDO, EFO, EO, CoatiOA
 #https://github.com/HaaLeo/swarmlib
 #import swarmlib
 import math
-
 import optimization_functions as of
 
 dimension_count = 2
-bench_count = 10
-max_epochs = 200
+bench_count = 5
+max_epochs = 50
 pop_size = 20
 
 # [func, name, min, max]
@@ -46,87 +45,97 @@ class FuncResult:
     self.name = name
     self.data = data
 
+class SheetRow:
+    def __init__(self, generation:int, positions:list[float], fitness:float, time:float):
+        self.generation = generation
+        self.positions = positions
+        self.fitness = fitness
+        self.time = time
+
 def benchmark_all(funcs):
-  results = []
+  wb = xlsxwriter.Workbook(f"exported/{algorithm_name}_wyniki.xlsx")
   for func in funcs:
     fitness_function = func[0]
     func_name = func[1]
-    min = func[2]
-    max = func[3]
+    min_val = func[2]
+    max_val = func[3]
     problem_dict = {
-        "bounds": FloatVar(lb=(min,) * dimension_count, ub=(max,) * dimension_count, name="delta"),
+        "bounds": FloatVar(lb=(min_val,) * dimension_count, ub=(max_val,) * dimension_count, name="delta"),
         "minmax": "min",
         "obj_func": fitness_function,
         "log_to": None,
         "save_population": True,
     }
     result = benchmark_single(func_name, problem_dict)
-    results.append(result)
-  create_excel_file(f"{algorithm_name}_wyniki", results)
-
-def create_excel_file(algorithm_name, results):
-  wb = xlsxwriter.Workbook(f"exported/{algorithm_name}.xlsx")
-  for res in results:
-    add_sheet_to_excel_workbook(wb, res.name, res.data)
+    add_sheet_to_excel_workbook(wb, func_name, result)
   wb.close()
-  pass
 
 data_names = ["Average","Standard deviation","Average time","Standard deviation time"]
-def add_sheet_to_excel_workbook(existing_workbook, sheetName, data):
-  sheet = existing_workbook.add_worksheet(sheetName)
+def add_sheet_to_excel_workbook(existing_workbook, sheet_name, sheet_rows:list[SheetRow]):
+  sheet = existing_workbook.add_worksheet(sheet_name)
   row = 0
   col = 0
-  for value in (data):
-      sheet.write(row, col, data_names[row])
-      sheet.write(row, col + 1, value)
-      row += 1
+  # A1  B1 C1 D1  E1   F1 G1  H1      I1       J1
+  # gen x1 x2 fit time |  avg std_dev avg_time std_dev_time
+  sheet.write(row, col + 0, "Generation")
+  sheet.write(row, col + 1, "x1")
+  sheet.write(row, col + 2, "x2")
+  sheet.write(row, col + 3, "Fitness")
+  sheet.write(row, col + 4, "Time")
+  sheet.write(row, col + 5, "")
+  sheet.write(row, col + 6, "Average Fitness")
+  sheet.write(row, col + 7, "StDev Fitness")
+  sheet.write(row, col + 8, "Average Time")
+  sheet.write(row, col + 9, "StDev Time")
+
+  row_count = len(sheet_rows)
+  row = 1
+  # G avg
+  sheet.write_formula(row, col + 6, f"=AVERAGE(D2:D{row_count+1})")
+  # H std_dev
+  sheet.write_formula(row, col + 7, f"=STDEV(D2:D{row_count + 1})")
+  # I avg time
+  sheet.write_formula(row, col + 8, f"=AVERAGE(E2:E{row_count + 1})")
+  # J std_dev time
+  sheet.write_formula(row, col + 9, f"=STDEV(E2:E{row_count + 1})")
+
+  for sheet_row in sheet_rows:
+        #A gen
+        sheet.write(row, col+0, sheet_row.generation)
+        #B x1
+        sheet.write(row,col+1, sheet_row.positions[0])
+        #C x2
+        sheet.write(row,col+2, sheet_row.positions[1])
+        #D fit
+        sheet.write(row, col + 3, sheet_row.fitness)
+        #E time
+        sheet.write(row, col + 4, sheet_row.time)
+        #F empty
+
+        row += 1
 
 def save_charts(model, algorithm_name, func_name, iteration):
   model.history.save_local_objectives_chart(filename=f"exported/{algorithm_name}/{func_name}/{iteration}/loc")
   model.history.save_local_best_fitness_chart(filename=f"exported/{algorithm_name}/{func_name}/{iteration}/lbfc")
   model.history.save_exploration_exploitation_chart(filename=f"exported/{algorithm_name}/{func_name}/{iteration}/eec")
 
-def save_history(model, algorithm_name, func_name, iteration):
-    current_best = model.history.list_current_best
-    flattened = [ind.solution.tolist() + [ind.target.fitness] for ind in current_best]
-    with open(f"exported/{algorithm_name}/{func_name}/{iteration}/current_best.csv","w",newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(flattened)
-
 def benchmark_single(func_name, problem_dict):
-  results = []
-  executions_times = []
+  sheet_rows = []
   print(f"Function: {func_name}")
+  gen = 1
   for i in range(bench_count):
+    tstart = time.time()
     result = model.solve(problem_dict)
-    tstart = datetime.datetime.now()
-    tend = datetime.datetime.now()
-    executions_times.append((tend-tstart).total_seconds())
+    tend = time.time()
     save_charts(model, algorithm_name, func_name, i)
-    save_history(model, algorithm_name, func_name, i)
 
-
-    results.append(result)
-
-  # średnia wyników
-  avg = sum(r.target.fitness for r in results)/bench_count
-
-  # odchylenie standardowe wyników
-  std = np.std([r.target.fitness for r in results])
-
-  # średnia czasów
-  avg_time = sum(executions_times)/bench_count
-  #avg_time = sum(model.history.list_epoch_time)/bench_count
-
-  #odchylenie standardowe czasów
-  std_time = np.std(executions_times)
-  #std_time = np.std(model.history.list_epoch_time)
-
-  return FuncResult(func_name, [avg, std, avg_time, std_time])
+    time_elapsed = tend-tstart
+    positions = result.solution.tolist()
+    fitness = result.target.fitness
+    sheet_row = SheetRow(gen,positions,fitness,time_elapsed)
+    sheet_rows.append(sheet_row)
+    gen = gen + 1
+  return sheet_rows
 
 benchmark_all(funcs)
 #shutil.make_archive(f"exported/{algorithm_name}", 'zip', f"exported/{algorithm_name}")
-
-#print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
-#print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
-
